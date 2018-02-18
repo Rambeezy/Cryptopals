292e8b06c6044f27663752bbe85ddf93b24eca5d
Joe Ditmar <joeditmar@gmail.com>
2018-02-18 13:16:09 -0600

    part 1 done

--- Set1.py
+++ Set1.py
@@ -1,2 +1,6 @@
-test    /ls.def funcname(parameter_list):
-    pass
+import codecs
+
+s = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d"
+bs = codecs.encode(codecs.decode(s, 'hex'), 'base64').decode()
+
+print(bs)
